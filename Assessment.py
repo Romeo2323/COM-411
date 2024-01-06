c888d807b2ed7d638ad396e52b464ace50b5b4ef
Romeo Mfuka <romeomfuka@Romeos-MacBook-Air.local>
2024-01-06 22:48:11 +0000

    sectiona2

--- Assessment.py
+++ Assessment.py
@@ -1,5 +1,12 @@
+
 # SectionA 1
 
 print("--------------------------")
 print("Disneyland Review Analyser")
 print("--------------------------")
+# SectionA 2
+
+def load_data(file_path):
+    with open (file_path) as csv_file:
+         csv_reader = csv.reader(csv_file)
+
