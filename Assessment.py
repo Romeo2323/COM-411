42544e39ecaa6e8e3a9abef81d7d55ed0271db3c
Romeo Mfuka <romeomfuka@Romeos-MacBook-Air.local>
2024-01-06 23:07:20 +0000

    section 2 complete

--- Assessment.py
+++ Assessment.py
@@ -7,6 +7,17 @@ print("--------------------------")
 # SectionA 2
 
 def load_data(file_path):
-    with open (file_path) as csv_file:
+    with open (file_path):
          csv_reader = csv.reader(csv_file)
+         next (csv_reader)
+
+         for line in csv_reader:
+             record.append(line)
+    print("Data has completed processing")
+
+# SectionA 3
+menu = {
+
+
+
 
